a066b767f7bfaae9746c4e35deb652da4e427ddb
John L. Lao <john.lao@gmail.com>
2020-11-11 12:39:09 +0800

    defer unlock

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	defer m.UnLock()
 
 	if mode == "Read" {
 		err = m.RLock()
@@ -48,8 +49,6 @@ func main() {
 	fmt.Println("Press ENTER to exit")
 	var stdin = bufio.NewReader(os.Stdin)
 	_, _, _ = stdin.ReadLine()
-
-	_ = m.UnLock()
 }
 
 
